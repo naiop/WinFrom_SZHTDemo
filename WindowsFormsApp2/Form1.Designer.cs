73e42094349058ee80c1ee4c0ed4d39c548a415f
FYG <token>
2021-04-07 09:02:49 +0800

    21-04-07 optimization

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -160,7 +160,6 @@ namespace WindowsFormsApp2
             this.dataGridView1.Size = new System.Drawing.Size(535, 226);
             this.dataGridView1.TabIndex = 5;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
-           
             // 
             // Column1
             // 
